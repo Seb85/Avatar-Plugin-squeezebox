eaa4bb75d6629d20eb63241dbc4b506e2f8b98ca
Seb85 <sebrenaud@hotmail.fr>
2019-03-30 18:17:17 +0100

    Add files via upload

---
+++ squeezebox/assets/infos.md
@@ -0,0 +1,3 @@
+# squeezebox
+
+Aucune description
--- squeezebox/squeezebox.js
+++ squeezebox/squeezebox.js
@@ -1,13 +1,13 @@
 var request = require('request');
+var Promise = require('q').Promise;
 
-require('colors');
 var _SqueezeboxConf;
 
 exports.init = function(){
 	
 	// table of properties
 	 _SqueezeboxConf = {
-		pathSqueezeboxApi:  Config.modules.squeezebox.html || '',
+		SqueezeboxHtml:  Config.modules.squeezebox.html || '',
 		ip : Config.modules.squeezebox.addrSqueezebox || ''
 	};
 	
@@ -17,19 +17,22 @@ exports.init = function(){
   
 exports.action = function(data, callback){
 	
-	if (!_SqueezeboxConf.pathSqueezeboxApi || !_SqueezeboxConf.ip ) {
+	if (!_SqueezeboxConf.SqueezeboxHtml || !_SqueezeboxConf.ip ) {
 		error("SqueezeBox:", "La configuration de SqueezeBox est manquante".red);
 		return callback();
 	}
 	
 	var tblCommand = {
-		mute : function() { requestSqueezeBoxMute(data.client, 'p0=mixer&p1=volume&p2=0&player=MAC')},
-		mutebis : function() { requestSqueezeBoxMute(data.client, 'p0=mixer&p1=volume&p2=100&player=MAC')},
-		play : function(){requestSqueezeBoxCmd(data.client, Config.modules.squeezebox.clients[room][data.action.value], "C'est en cours.|Comme si c'était fait.|magique.")},
+		play : function(){requestSqueezeBoxCmd(data.client, Config.modules.squeezebox.clients[room][data.action.value], "C'est en cours.|Comme si c'était fait.|magique.|Et C'est parti pour le show.")},
 		pause : function(){requestSqueezeBoxCmd(data.client, Config.modules.squeezebox.clients[room][data.action.value], "OK.")},
-		stop : function(){requestSqueezeBoxCmd(data.client, Config.modules.squeezebox.clients[room][data.action.value], "Bien monsieur.")}
+		stop : function(){requestSqueezeBoxCmd(data.client, Config.modules.squeezebox.clients[room][data.action.value], "Bien monsieur.")},
+		suivant : function(){requestSqueezeBoxCmd(data.client, Config.modules.squeezebox.clients[room][data.action.value], "Suivant.")},
+		precedent : function(){requestSqueezeBoxCmd(data.client, Config.modules.squeezebox.clients[room][data.action.value], "Précédent.")},
+		sondown : function(){requestSqueezeBoxCmd(data.client, Config.modules.squeezebox.clients[room][data.action.value], "OK.")},
+		sonup : function(){requestSqueezeBoxCmd(data.client, Config.modules.squeezebox.clients[room][data.action.value], "OK.")},
+		deezer : function(){requestSqueezeBoxDeezer(data, client, Config.modules.squeezebox.clients[room][data.action.value])}
 	};
-	
+	let client = setClient(data);
 	var room = setClient(data);
 	info("SqueezeBox command:", data.action.command.yellow, "From:", data.client.yellow, "To:", room.yellow);
 	tblCommand[data.action.command]();
@@ -51,16 +54,12 @@ var setClient = function (data) {
 	return client;
 }
 
-function requestSqueezeBoxMute (client, value, txt) {
+function requestSqueezeBoxCmd (client, value, txt) {
 	
 	
-	var uri = _SqueezeboxConf.ip + _SqueezeboxConf.pathSqueezeboxApi + value;
-	
-	info('uri:', uri.yellow);
-	
-	request({
-		url: uri
-	},
+	var url = _SqueezeboxConf.ip + _SqueezeboxConf.SqueezeboxHtml + value;
+	info(url)
+	http_request(url),
 	function(state) {
 		if (txt) {
 			Avatar.speak(txt, client, function() {
@@ -68,48 +67,382 @@ function requestSqueezeBoxMute (client, value, txt) {
 			});
 			} else {
 			var answer = !state;
-			info(answer);
+			Avatar.Speech.end(client);
 		}
 		},
 	function (err, response) {
 		if (err || response.statusCode != 200) {
 			info('Error: Callback request'.red);
-			Avatar.Speech.end(client);
 			return callback(false);
 		}
 	   
+	};
+	
+}
+
+function requestSqueezeBoxDeezer(data, client, value) {
+	var url = _SqueezeboxConf.ip + '/anyurl?p0=playlist&p1=clear&' + value
+	http_request(url)
+    Avatar.askme("Tu veux rechercher un artiste, un album, un titre, une playlist ou bien le flo ?", data.client,
+	{
+		"*": "generic",
+        "terminer": "done"
+	},0, function (answer, end) {
+			
+			if (answer && answer.indexOf('generic') != -1) {
+              end(data.client);
+              answer = answer.split(':')[1];
+
+              if (answer.indexOf('artiste') != -1) {
+                return deezerartiste(data, client, value);
+              }
+
+              if (answer.indexOf('album') != -1) {
+                return deezeralbum(data, client, value);
+              }
+
+              if (answer.indexOf('titre') != -1) {
+                return deezertitre(data, client, value);
+              }
+			  
+			  if (answer.indexOf('playlist') != -1) {
+                return deezerplaylist(data, client, value);
+              }
+			  
+			  if (answer.indexOf('flow') != -1) {
+				Avatar.speak("C'est parti.", data.client, function () {
+                });
+				var uri = _SqueezeboxConf.ip + _SqueezeboxConf.SqueezeboxHtml + 'p0=playlist&p1=play&p2=deezer%3A%2F%2Fflow.dzr&p3=Deezer%20Flow&' + value;
+	
+				info('uri:', uri.yellow);
+	
+				request({
+					url: uri
+				},
+				function (err, response) {
+					if (err || response.statusCode != 200) {
+						info('Error: Callback request'.red);
+						return callback(false);
+					}
+	   
+				});
+				end(data.client, true);
+				return;
+              }
+
+              return Avatar.speak("Je suis désolé, je n'ai pas compris.", data.client, function(){
+                  deezer(data, client)
+              });
+          }
+
+          // Grammaire fixe
+          switch(answer) {
+            case "done":
+            default:
+                Avatar.speak("Terminé", data.client, function(){
+                    end(data.client, true);
+                });
+         }
+      })		
+}	
+
+function deezerartiste(data, client, value) {
+				Avatar.askme("Quel artiste ?", data.client,
+				{
+					"*": "generic",
+                    "terminer": "done"
+				},0, function (answer, end) {
+					if (answer && answer.indexOf('generic') != -1) {
+					answer = answer.split(':')[1];
+					answer = answer.replace('l\'','');
+					answer = answer.replace('é','e');
+					answer = answer.replace('è','e');
+					answer = answer.replace('ê','e');
+					answer = answer.replace('ç','c');
+					answer = answer.replace('ö','o');
+					answer = answer.replace('à','a');
+					answer = answer.replace('ï','i');
+					answer = answer.replace(':','');
+					answer = answer.replace('.','');
+					answer = answer.replace(',','');
+					answer = answer.replace(';','');
+					answer = answer.replace('?','');
+					answer = answer.replace('!','');
+					answer = answer.replace('&','');
+					answer = answer.replace('angel','angele');
+					answer = answer.replace('jane','jain');
+					answer = answer.replace(answer[0], answer[0].toUpperCase());
+					info(answer)
+					Avatar.speak("C'est parti.", data.client, function () {
+					});
+					var url = _SqueezeboxConf.ip + '/plugins/deezer/index.html?' + value + '&sess=&index=0f41dc65.0.0&submit=Recherche&q=' + answer
+					http_request(url)
+					.then(body => scraperartiste(body))
+					.then(function(index) { 
+					var url = _SqueezeboxConf.ip + '/plugins/deezer/index.html?' + value + '&' + index + '&sess='
+					http_request(url)
+					.then(body => scraperall(body, value))
+					})
+					end(data.client, true);
+					return;
+					}
+					
+					// Grammaire fixe
+					switch(answer) {
+					case "done":
+					default:
+						Avatar.speak("Terminé", data.client, function(){
+							end(data.client, true);
+					});
+			}
+	})
+}
+
+function deezeralbum(data, client, value) {
+				Avatar.askme("Quel album ?", data.client,
+				{
+					"*": "generic",
+                    "terminer": "done"
+				},0, function (answer, end) {
+					if (answer && answer.indexOf('generic') != -1) {
+					answer = answer.split(':')[1];
+					answer = answer.replace('l\'','');
+					answer = answer.replace('é','e');
+					answer = answer.replace('è','e');
+					answer = answer.replace('ê','e');
+					answer = answer.replace('ç','c');
+					answer = answer.replace('ö','o');
+					answer = answer.replace('à','a');
+					answer = answer.replace('ï','i');
+					answer = answer.replace(':','');
+					answer = answer.replace('.','');
+					answer = answer.replace(',','');
+					answer = answer.replace(';','');
+					answer = answer.replace('?','');
+					answer = answer.replace('!','');
+					answer = answer.replace('&','');
+					answer = answer.replace('angel','angele');
+					answer = answer.replace('jane','jain');
+					answer = answer.replace(answer[0], answer[0].toUpperCase());
+					info(answer)
+					Avatar.speak("C'est parti.", data.client, function () {
+					});
+					var url = _SqueezeboxConf.ip + '/plugins/deezer/index.html?' + value + '&sess=&index=27df761e.0.1&submit=Recherche&q=' + answer
+					http_request(url)
+					.then(body => scraperalbum(body))
+					.then(function(index) { 
+					var url = _SqueezeboxConf.ip + '/plugins/deezer/index.html?' + value + '&' + index + '&sess='
+					http_request(url)
+					.then(body => scraperall(body, value))
+					})
+					end(data.client, true);
+					return;
+					}
+					
+					// Grammaire fixe
+					switch(answer) {
+					case "done":
+					default:
+						Avatar.speak("Terminé", data.client, function(){
+							end(data.client, true);
+					});
+			}
+	})
+}
+
+function deezertitre(data, client, value) {
+				Avatar.askme("Quel titre ?", data.client,
+				{
+					"*": "generic",
+                    "terminer": "done"
+				},0, function (answer, end) {
+					if (answer && answer.indexOf('generic') != -1) {
+					answer = answer.split(':')[1];
+					answer = answer.replace('l\'','');
+					answer = answer.replace('é','e');
+					answer = answer.replace('è','e');
+					answer = answer.replace('ê','e');
+					answer = answer.replace('ç','c');
+					answer = answer.replace('ö','o');
+					answer = answer.replace('à','a');
+					answer = answer.replace('ï','i');
+					answer = answer.replace(':','');
+					answer = answer.replace('.','');
+					answer = answer.replace(',','');
+					answer = answer.replace(';','');
+					answer = answer.replace('?','');
+					answer = answer.replace('!','');
+					answer = answer.replace('&','');
+					answer = answer.replace('angel','angele');
+					answer = answer.replace('jane','jain');
+					answer = answer.replace(answer[0], answer[0].toUpperCase());
+					info(answer)
+					Avatar.speak("C'est parti.", data.client, function () {
+					});
+					var url = _SqueezeboxConf.ip + '/plugins/deezer/index.html?' + value + '&sess=&index=a319d32b.0.2&submit=Recherche&q=' + answer
+					http_request(url)
+					.then(body => scrapertitre(body, value))
+					end(data.client, true);
+					return;
+					}
+					
+					// Grammaire fixe
+					switch(answer) {
+					case "done":
+					default:
+						Avatar.speak("Terminé", data.client, function(){
+							end(data.client, true);
+					});
+			}
+	})
+}
+
+function deezerplaylist(data, client, value) {
+				Avatar.askme("Vous avez le choix entre ces playlist. Soirée, Sport ?", data.client,
+				{
+					"*": "generic",
+                    "terminer": "done"
+				},0, function (answer, end) {
+					if (answer && answer.indexOf('generic') != -1) {
+					end(data.client);
+					answer = answer.split(':')[1];
+
+					if (answer.indexOf('soirée') != -1) {
+						end(data.client);
+						info(answer)
+						Avatar.speak("C'est parti.", data.client, function () {
+						});
+						var url = _SqueezeboxConf.ip + '/plugins/deezer/index.html?' + value + '&sess=&index=a8fde8a9.3.0&sess='
+						http_request(url)
+						.then(body => scraperplaylist(body, answer, value))
+						end(data.client, true);
+						return;
+					}
+			  
+					if (answer.indexOf('sport') != -1) {
+						end(data.client);
+						info(answer)
+						Avatar.speak("C'est parti.", data.client, function () {
+						});
+						var url = _SqueezeboxConf.ip + '/plugins/deezer/index.html?' + value + '&sess=&index=a8fde8a9.3.0&sess='
+						http_request(url)
+						.then(body => scraperplaylist(body, answer, value))
+						end(data.client, true);
+						return;
+					}
+
+					return Avatar.speak("Je suis désolé, je n'ai pas compris.", data.client, function(){
+						deezerplaylist(data, client)
+					});
+				}
+
+				// Grammaire fixe
+				switch(answer) {
+					case "done":
+					default:
+						Avatar.speak("Terminé", data.client, function(){
+							end(data.client, true);
+						});
+				}
+		})
+}
+
+function scraperartiste(body) {
+	return new Promise(function (resolve, reject) {
+		var index = body;
+		index = index.split('<div class="browseItemDetail">')[1];
+		index = index.split('<a href="index.html?')[1];
+		index = index.split('.0&')[0];
+		index = index + '.1';
+		if (!index) {
+			Avatar.speak("Désolé je n'ai pas trouvé.", data.client, function(){
+			});
+		}
+		resolve (index);
 	});
 	
 }
 
-function requestSqueezeBoxCmd (client, value, txt) {
-	
-	
-	var uri = _SqueezeboxConf.ip + _SqueezeboxConf.pathSqueezeboxApi + value;
-	
-	info('uri:', uri.yellow);
-	
-	request({
-		url: uri
-	},
-	function(state) {
-		if (txt) {
-			Avatar.speak(txt, client, function() {
-				Avatar.Speech.end(client);
+function scraperalbum(body) {
+	return new Promise(function (resolve, reject) {
+		var index = body;
+		index = index.split('<div class="browseItemDetail">')[1];
+		index = index.split('<a href="index.html?')[1];
+		index = index.split('&')[0];
+		
+		if (!index) {
+			Avatar.speak("Désolé je n'ai pas trouvé.", data.client, function(){
 			});
-			} else {
-			var answer = !state;
-			Avatar.Speech.end(client);
-			info(answer);
 		}
-		},
-	function (err, response) {
-		if (err || response.statusCode != 200) {
-			info('Error: Callback request'.red);
-			Avatar.Speech.end(client);
-			return callback(false);
+		resolve (index);
+	});
+	
+}
+
+function scrapertitre(body, value) {
+	return new Promise(function (resolve, reject) {
+		var index = body;
+		index = index.split('title="Lecture"/></a>')[1];
+		index = index.split('SqueezeJS.Controller.urlRequest(')[1];
+		index = index.split("'")[1];
+		index = index.split("'")[0];
+		if (!index) {
+			Avatar.speak("Désolé je n'ai pas trouvé.", data.client, function(){
+			});
 		}
-	   
+		var url = _SqueezeboxConf.ip + index
+		http_request(url)
+		var url = _SqueezeboxConf.ip + _SqueezeboxConf.SqueezeboxHtml + 'p0=play&p1=1&' + value
+		http_request(url)
+		resolve (index);
+	});
+	
+}
+
+function scraperplaylist(body, answer, value) {
+		var index = body;
+		var answer = answer.substring(1, answer.length0);
+		index = index.split(answer)[0];
+		var index = index.split(/html\?+/).pop();
+		index = index.split('&')[0];
+		if (!index) {
+			Avatar.speak("Désolé je n'ai pas trouvé.", data.client, function(){
+			});
+		}
+		var url = _SqueezeboxConf.ip + '/plugins/deezer/index.html?' + value + '&' + index + '&sess='
+		http_request(url)
+		.then(body => scraperall(body, value))
+}
+
+function scraperall(body, value) {
+	return new Promise(function (resolve, reject) {
+		var regex = /\/anyurl?.*', 1\)/g
+		var index = body.match(regex);
+		for(let a of index) 
+			{
+			var index2 = a.substring(0, a.length-5);
+			var url = _SqueezeboxConf.ip + index2
+			http_request(url)
+			};
+			var url = _SqueezeboxConf.ip + _SqueezeboxConf.SqueezeboxHtml + 'p0=play&p1=1&' + value
+			http_request(url)
+		resolve (index);
+	});
+}
+
+function http_request (url) {
+	return new Promise(function (resolve, reject) {
+		
+		var request = require('request');
+		request({ 'uri' : url}, function (err, response, body) {
+		
+			if (err || response.statusCode != 200) {
+			  return reject ('Désolé je n\'ai pas trouvé de vie de merde');
+			}
+
+			resolve(body);
+		});
+		
 	});
 	
 }
